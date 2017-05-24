d6aaa45b03b32146f97308b272d63ccba6c84bab
Martin Eldevig Olsen <meol16@student.aau.dk>
2017-05-24 23:40:52 +0200

    Began wave chunk handling

---
+++ MusicAnalysis/MusicAnalysis/TESTAREA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MusicAnalysis
+{
+    static class TESTAREA
+    {
+        public static void TestMethod()
+        {
+            Stream fileStream = new FileStream(@"C:\Users\melde\Downloads\Lamb of God - Redneck.wav", FileMode.Open);
+            BinaryReader binaryRead = new BinaryReader(fileStream);
+            fileStream.Position = 0;
+
+            byte[] chunkDescriptor = binaryRead.ReadBytes(4);
+
+            binaryRead.Close();
+            fileStream.Close();
+        }
+    }
+}
---
+++ MusicAnalysis/MusicAnalysis/WaveFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicAnalysis
+{
+    class WaveFile
+    {
+        private string _filePath;
+
+        public WaveFile(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        
+    }
+}
